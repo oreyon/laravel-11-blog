c88f4559e212a1dcee35c8b190e952a966cd4bb8
oreoataya <dontrichpeopledifficulut@gmail.com>
2024-06-11 07:15:56 +0800

    refactoring cloudbuild.yaml & deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# To run this deploy.sh file, you need to give it execute permission
+# To give execute permission, run the following command
+# go to the directory where the deploy.sh file is located and run the following command
+# chmod +x deploy.sh
+# chmod +x /var/www/laravel/deploy.sh
+
+
+# Set up environment variables
+APP_PATH=${APP_PATH:-'/var/www/laravel'}
+
 # Continuous Integration
 echo "Build..."
-cd src || exit
+cd $APP_PATH || exit
 rm -rf vendor
 composer install --no-dev --optimize-autoloader
+
+echo "Test..."
+vendor/bin/phpunit
 
 # Continuous Deployment
 echo "Deploy..."
 rsync --exclude ".git" --exclude "storage" -av --delete . $APP_PATH/.
 
-cd $APP_PATH/ || exit
+cd $APP_PATH || exit
 
 echo "Clear cache..."
 php artisan cache:clear
 
 echo "Database Migration..."
-php artisan migrate:fresh --seed
+php artisan migrate --force
 
 echo "Reload PHP & Nginx"
 sudo systemctl reload php8.3-fpm
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
+// make test load homepage
+test('homepage loads', function () {
     $response = $this->get('/');
-
     $response->assertStatus(200);
 });
