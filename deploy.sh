8e016fd3ee6696e1adf51229048beeb7f3c83e8d
oreoataya <dontrichpeopledifficulut@gmail.com>
2024-06-11 09:52:59 +0800

    revision cloudbuild.yaml
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml
    
    revision cloudbuild.yaml

--- deploy.sh
+++ deploy.sh
@@ -7,25 +7,59 @@
 # chmod +x /var/www/laravel/deploy.sh
 
 
-# Set up environment variables
-APP_PATH=${APP_PATH:-'/var/www/laravel'}
+echo "SETTING ENVIRONMENT VARIABLES"
+APP_PATH=${_ENV_APP_PATH:-''}
+ENV_ZONE=${_ENV_ZONE:-''}
+ENV_INST_USER=${_ENV_INST_USER:-''}
+ENV_INST_NAME=${_ENV_INST_NAME:-''}
+ENV_APP_PATH=${_ENV_APP_PATH:-''}
+ENV_PULL_PATH=${_ENV_PULL_PATH:-''}
+ENV_BRANCH=${_ENV_BRANCH:-''}
+echo "APP_PATH: $APP_PATH"
+echo "ENV_ZONE: $ENV_ZONE"
+echo "ENV_INST_USER: $ENV_INST_USER"
+echo "ENV_INST_NAME: $ENV_INST_NAME"
+echo "ENV_APP_PATH: $ENV_APP_PATH"
+echo "ENV_PULL_PATH: $ENV_PULL_PATH"
+echo "ENV_BRANCH: $ENV_BRANCH"
+
+# source /home/c663bsy4320/.nvm/nvm.sh
+NVM=/home/$_ENV_INST_USER/.nvm/nvm.sh
+source $NVM
+echo "NVM Location: $NVM"
+
 chmod +x deploy.sh
 
 # Continuous Integration
 echo "Build..."
-cd $APP_PATH || exit
+cd $APP_PATH
+
+git stash
+git fetch --all
+git reset --hard origin/main  
 git pull # Pull the latest changes from the repository
+
+# sudo chown -R $(whoami):$(whoami) $APP_PATH
+sudo chown root:root .
+sudo chmod 777 .
+
 rm -rf vendor
 composer install --no-dev --optimize-autoloader
-npm install
-npm build
+composer update
+
+$NVM npm install
+$NVM npm run build
+
+sudo chown -R www-data:www-data $APP_PATH/storage
+
 
 echo "Test..."
-/vendor/bin/phpunit
+php artisan test
 
 # Continuous Deployment
 echo "Deploy..."
 rsync --exclude ".git" --exclude "storage" -av --delete . $APP_PATH/.
+# sudo chown -R www-data:www-data $APP_PATH/storage
 
 cd $APP_PATH || exit
 
@@ -35,6 +69,10 @@ php artisan cache:clear
 echo "Database Migration..."
 php artisan migrate --force
 
+
 echo "Reload PHP & Nginx"
 sudo systemctl reload php8.3-fpm
 sudo systemctl reload nginx
+
+# If there were stashed changes, apply them back
+git stash pop || true
