d0f70b88458f6566e52dd4151507a2357d601c3c
oreoataya <dontrichpeopledifficulut@gmail.com>
2024-06-13 00:23:59 +0800

    Revision deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# To run this deploy.sh file, you need to give it execute permission
-# To give execute permission, run the following command
-# go to the directory where the deploy.sh file is located and run the following command
-# chmod +x deploy.sh
-# chmod +x /var/www/laravel/deploy.sh
-
-
+echo "STARTING DEPLOYMENT"
 echo "SETTING ENVIRONMENT VARIABLES"
 APP_PATH=${_ENV_APP_PATH:-''}
 ENV_ZONE=${_ENV_ZONE:-''}
@@ -23,51 +17,50 @@ echo "ENV_APP_PATH: $ENV_APP_PATH"
 echo "ENV_PULL_PATH: $ENV_PULL_PATH"
 echo "ENV_BRANCH: $ENV_BRANCH"
 
-# Ensure NVM script is executable
+echo "SET PERMISSION NVM"
 chmod +x /home/$ENV_INST_USER/.nvm/nvm.sh
 
-# source /home/c663bsy4320/.nvm/nvm.sh
+echo "DETECTING NVM"
 NVM=/home/$_ENV_INST_USER/.nvm/nvm.sh
 source $NVM
 echo "NVM Location: $NVM"
 
 
-# Continuous Integration
+echo "DETECTING PHP"
 echo "Build..."
 cd $APP_PATH || exit
 sudo rm -rf vendor
 
-echo "Change ownership root"
+echo "CHANGE OWNERSHIP"
 sudo chown root:root $APP_PATH
 
-echo "Composer Install"
+# INSTALLING DEPENDENCIES
+echo "INSTALLING DEPENDENCIES"
+echo "COMPOSER INSTALL"
 composer install --no-dev --optimize-autoloader
 composer update
 
-echo "NPM INSTALL"
+echo "INSTALLING NPM"
 npm install
 echo "NPM BUILD"
 npm run build
 
-echo "Test..."
+# TESTING
+echo "TESTING CODE"
 cd $APP_PATH || exit
 php artisan test
 
-# Continuous Deployment
-echo "Deploy..."
+# DEPLOYING CODE
+echo "DEPLOYING CODE"
 rsync --exclude ".git" --exclude "storage" -av --delete . $APP_PATH/.
 
 cd $APP_PATH || exit
 
-echo "Clear cache..."
+echo "CLEARING CACHE"
 php artisan cache:clear
 
-echo "Database Migration..."
-php artisan migrate --force
-
-echo "Turn files permissions..."
-sudo chown -R $USER:$USER $APP_PATH
-sudo chmod -R 775 $APP_PATH
+echo "MIGRATING DATABASE"
+php artisan migrate:fresh --seed
 
 echo "Ensure the web server can write to the storage and cache directories"
 sudo chown -R www-data:www-data $APP_PATH/storage
@@ -79,3 +72,4 @@ sudo systemctl reload nginx
 
 # If there were stashed changes, apply them back
 git stash pop || true
+echo "DEPLOYMENT COMPLETE"
