ff7556162f85dceba1a780a17c4152f1a561bc40
oreoataya <dontrichpeopledifficulut@gmail.com>
2024-06-12 23:31:00 +0800

    Revision deploy.sh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <x-slot:titleSlot>{{ $title }}</x-slot:titleSlot>
-    <h3 class="text-xl">This is homepage revision with secret manager - 1</h3>
+    <h3 class="text-xl">This is homepage revision with secret manager - 10</h3>
 </x-layout>
